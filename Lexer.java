da1c57b840633f760ce847c0c4cccf063586e553
Tommy Avetisyan <tomavetisyan95@gmail.com>
2020-02-25 13:37:50 -0800

    Single int tokens

--- Lexer.java
+++ Lexer.java
@@ -4,13 +4,16 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Lexer {
-    static String[] reserved = { "Int", "String", "Bool", "Void", "this", "print", "println", "new", "for", "while",
-            "if", "else", "return", "public", "private", "protected", "class", "constructor" };
-
-    public static String getAtom(String s, int i) {
+    static String[] reserved = { "Int", "String", "Bool", 
+                                "Void", "this", "print", 
+                                "println", "new", "for", 
+                                "while", "if", "else", 
+                                "return", "public", "private",
+                                "protected", "class", "constructor" };
+    public static String getAtom(String s, int i){
         int j = i;
-        for (; j < s.length();) {
-            if (Character.isLetter(s.charAt(j))) {
+        for (; j < s.length();){
+            if (Character.isLetter(s.charAt(j))){
                 j++;
             } else {
                 return s.substring(i, j);
@@ -18,52 +21,56 @@ public class Lexer {
         }
         return s.substring(i, j);
     }
-
-    public static List<Token> lex(String input) {
-
+    public static List<Token> lex(String input){
         List<Token> result = new ArrayList<Token>();
-        for (int i = 0; i < input.length();) {
-            switch (input.charAt(i)) {
-                case '(': {
-                    result.add(new LeftParenToken());
-                    i++;
-                    break;
-                }
-                case ')': {
-                    result.add(new RightParenToken());
-                    i++;
-                    break;
-                }
-                case '+':
-                case '-':
-                case '*':
-                case '/':
-                case '%':
-                case '<':
-                case '>':
-                case '=': {
-                    result.add(new Binary_OPToken(String.valueOf(input.charAt(i))));
-                    i++;
-                    break;
-                }
-                default:
-                    if (Character.isWhitespace(input.charAt(i))) {
+        for (int i = 0; i < input.length();){
+            if(Character.isDigit((input.charAt(i)))){
+                result.add(new IntegerToken(Character.getNumericValue(input.charAt(i))));
+                i++;
+            }else{
+                switch (input.charAt(i)){
+                    case '(':{
+                        result.add(new LeftParenToken());
                         i++;
-                    } else {
-                        String atom = getAtom(input, i);
-                        i += atom.length();
-                        if (Arrays.asList(reserved).contains(atom)) {
-                            result.add(new ReservedToken(atom));
-                        }
-                        // result.add(new Token(Type.ATOM, atom));
+                        break;
                     }
-                    break;
+                    case ')':{
+                        result.add(new RightParenToken());
+                        i++;
+                        break;
+                    }
+                    case '+':
+                    case '-':
+                    case '*':
+                    case '/':
+                    case '%':
+                    case '<':
+                    case '>':
+                    case '=':{
+                        result.add(new Binary_OPToken(String.valueOf(input.charAt(i))));
+                        i++;
+                        break;
+                    }
+                    default:
+                        if(Character.isWhitespace(input.charAt(i))){
+                            i++;
+                        }else{
+                            String atom = getAtom(input, i);
+                            i += atom.length();
+                            if (Arrays.asList(reserved).contains(atom)){
+                                result.add(new ReservedToken(atom));
+                            }
+                            // result.add(new Token(Type.ATOM, atom));
+                        }
+                        break;
+                }
             }
-        }
+            }
+            
         return result;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
         Scanner in = new Scanner(System.in);
         String name = in.nextLine();
 
