afa53579b8bfc3b2b398513df37de46003f74da7
Tomavetisyan <54915660+Tomavetisyan@users.noreply.github.com>
2020-05-11 22:20:03 -0700

    Expression Generation

--- src/main/java/codeGenerator/CodeGenerator.java
+++ src/main/java/codeGenerator/CodeGenerator.java
@@ -30,5 +30,106 @@ public class CodeGenerator {
     private void generateProgramCode(){
 
     }
+    private void generateExpressionCode(Expression exp, ArrayList<String> currentList){
+        if (exp instanceof DivisionExp){
+            DivisionExp div =(DivisionExp) exp;
+            currentList.add("(");
+            generateExpressionCode(div.exp1,currentList);
+            currentList.add("/");
+            generateExpressionCode(div.exp2,currentList);
+            currentList.add(")");
+        } else if (exp instanceof EqualEqualExp){
+            EqualEqualExp equalEqual =(EqualEqualExp) exp;
+            currentList.add("(");
+            generateExpressionCode(equalEqual.exp1,currentList);
+            currentList.add("==");
+            generateExpressionCode(equalEqual.exp2,currentList);
+            currentList.add(")");
+        } else if (exp instanceof GreaterThanExp){
+            GreaterThanExp greaterThan =(GreaterThanExp) exp;
+            currentList.add("(");
+            generateExpressionCode(greaterThan.exp1,currentList);
+            currentList.add(">");
+            generateExpressionCode(greaterThan.exp2,currentList);
+            currentList.add(")");
+        } else if (exp instanceof GreaterThanOrEqualExp){
+            GreaterThanOrEqualExp greaterThanorEqual =(GreaterThanOrEqualExp) exp;
+            currentList.add("(");
+            generateExpressionCode(greaterThanorEqual.exp1,currentList);
+            currentList.add(">=");
+            generateExpressionCode(greaterThanorEqual.exp2,currentList);
+            currentList.add(")");
+        } else if (exp instanceof IntegerExp){
+            IntegerExp integer =(IntegerExp) exp;
+            currentList.add("(");
+            currentList.add(Integer.toString(integer.value));
+            currentList.add(")");
+        } else if (exp instanceof LessThanExp){
+            LessThanExp lessThan =(LessThanExp) exp;
+            currentList.add("(");
+            generateExpressionCode(lessThan.exp1,currentList);
+            currentList.add("<");
+            generateExpressionCode(lessThan.exp2,currentList);
+            currentList.add(")");
+        } else if (exp instanceof LessThanOrEqualExp){
+            LessThanOrEqualExp lessThanorEqual =(LessThanOrEqualExp) exp;
+            currentList.add("(");
+            generateExpressionCode(lessThanorEqual.exp1,currentList);
+            currentList.add("<=");
+            generateExpressionCode(lessThanorEqual.exp2,currentList);
+            currentList.add(")");
+        } else if (exp instanceof MethodCallExp){
+            
+        } else if (exp instanceof MinusExp){
+            MinusExp min =(MinusExp) exp;
+            currentList.add("(");
+            generateExpressionCode(min.exp1,currentList);
+            currentList.add("-");
+            generateExpressionCode(min.exp2,currentList);
+            currentList.add(")");
+        } else if (exp instanceof ModulusExp){
+            ModulusExp mod =(ModulusExp) exp;
+            currentList.add("(");
+            generateExpressionCode(mod.exp1,currentList);
+            currentList.add("%");
+            generateExpressionCode(mod.exp2,currentList);
+            currentList.add(")");
+        } else if (exp instanceof MultiplicationExp){
+            MultiplicationExp mult =(MultiplicationExp) exp;
+            currentList.add("(");
+            generateExpressionCode(mult.exp1,currentList);
+            currentList.add("*");
+            generateExpressionCode(mult.exp2,currentList);
+            currentList.add(")");
+        } else if (exp instanceof NewExp){
 
+        } else if (exp instanceof ParenthesizedExp){
+            ParenthesizedExp paren =(ParenthesizedExp) exp;
+            currentList.add("(");
+            generateExpressionCode(paren.body,currentList);
+            currentList.add(")");
+        } else if (exp instanceof PlusExp){
+            PlusExp plus =(PlusExp) exp;
+            currentList.add("(");
+            generateExpressionCode(plus.exp1,currentList);
+            currentList.add("+");
+            generateExpressionCode(plus.exp2,currentList);
+            currentList.add(")");
+        } else if (exp instanceof StringExp){
+            StringExp string =(StringExp) exp;
+            currentList.add("\"");
+            currentList.add(string.value);
+            currentList.add("\"");
+        } else if (exp instanceof ThisExp){
+
+        } else if (exp instanceof VariableExp){
+            VariableExp var =(VariableExp) exp;
+            currentList.add("(");
+            currentList.add(var.name);
+            currentList.add(")");
+        } else {
+            throws codeGeneratorException
+        }
+
+    }
 }
